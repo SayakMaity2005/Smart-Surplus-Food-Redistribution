4de0610f987cb5191c28c4f10f063596ecd8da1d
Sayak Maity <sayak6481@gmail.com>
2025-12-02 23:30:27 +0530

    bcrypt maxlength set for password

--- backend/SurplusApp/surplus/security.py
+++ backend/SurplusApp/surplus/security.py
@@ -4,6 +4,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Hash:
     def hash_password(password: str) -> str:
+        password = password[:72]  # bcrypt max length
         return pwd_context.hash(password)
 
     def verify_password(input_password: str, actual_hashed_password: str) -> bool:
