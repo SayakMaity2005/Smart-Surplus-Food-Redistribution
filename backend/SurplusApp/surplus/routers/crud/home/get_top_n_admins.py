6a834827a452a3dd99228a0e0b25b87d44a7d3f5
Sayak Maity <sayak6481@gmail.com>
2026-01-04 00:13:21 +0530

    Top n admin data collection problem fix.

--- backend/SurplusApp/surplus/routers/crud/home/get_top_n_admins.py
+++ backend/SurplusApp/surplus/routers/crud/home/get_top_n_admins.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timezone
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from surplus.auth import get_db
 from surplus import models
 from sqlalchemy.orm import Session
+from sqlalchemy import extract
 
 router = APIRouter()
 
@@ -22,15 +24,15 @@ async def get_top_n_admins(request: RequestSchema, db: Session = Depends(get_db)
     highest_items_provided_monthly_data = db.query(models.MonthlyData).order_by(models.MonthlyData.items_provided.desc()).first()
     highest_items_provided = highest_items_provided_monthly_data.items_provided
     if highest_items_provided == 0: highest_items_provided = 1
-    top_n_items_provided_monthly_data = db.query(models.Admin).order_by(
-        ((0.75*(models.Admin.monthly_data.impact_score))+(0.25*((100.0*(models.Admin.monthly_data.items_provided))/highest_items_provided))).desc()
+    top_n_items_provided_monthly_data = db.query(models.MonthlyData).filter(
+        extract("month", models.MonthlyData.date) == datetime.now(timezone.utc).month,
+        extract("year", models.MonthlyData.date) == datetime.now(timezone.utc).year
+    ).order_by(
+        ((0.75*(models.MonthlyData.impact_score))+(0.25*((100.0*(models.MonthlyData.items_provided))/highest_items_provided))).desc()
     ).limit(request.num_admin).all()
     top_n_admins: list[TopAdminData] = []
-    # for monthly_data in top_n_items_provided_monthly_data:
-    #     top_n_admins.append(TopAdminData(monthly_data.admin.name, monthly_data.admin.profile_pic_url, monthly_data.impact_score, monthly_data.items_provided))
-
-    for admin_data in top_n_items_provided_monthly_data:
-        top_n_admins.append(TopAdminData(admin_data.name, admin_data.profile_pic_url, admin_data.monthly_data.impact_score, admin_data.monthly_data.items_provided))
+    for monthly_data in top_n_items_provided_monthly_data:
+        top_n_admins.append(TopAdminData(monthly_data.admin.name, monthly_data.admin.profile_pic_url, monthly_data.impact_score, monthly_data.items_provided))
 
     return {"message": "Top admins data collected", "data": top_n_admins}
 
